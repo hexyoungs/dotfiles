f2e00d4faf6a7e3a1e5d2be2c3e36e2706f17557
hexyoungs <chuxdesign@hotmail.com>
2020-10-14 11:39:04 +0800

    update

---
+++ .config/scripts/bar.sh
@@ -0,0 +1,206 @@
+#!/bin/ksh
+
+red="%{F#cd0000}"
+yellow="%{F#cdcd00}"
+black="%{F#000000}"
+
+wifi_ssid=$(cat <<EOF
+\$1 ~ "ieee80211" {
+	split(\$0, chars, "");
+	match(\$0, /join .* chan/);
+	ssid = substr(\$0, RSTART + 5, RLENGTH - 10)
+	gsub(/\"/, "", ssid)
+	print ssid
+}
+EOF
+)
+wifi_sig=$(cat <<EOF
+\$1 ~ "ieee80211" {
+	split(\$0, chars, "");
+	match(\$0, /[0-9]+%/);
+	val=substr(\$0, RSTART, RLENGTH)
+	if (val == "") {
+		printf \$(NF - 9)
+	} else {
+		printf "%3d%%", val
+	}
+}
+EOF
+)
+
+mem_fix=$(cat <<EOF
+{
+	split(\$3, real, "/")
+	print "Mem: U: " real[2] " F: " \$6 " C: " \$8
+}
+EOF
+)
+
+temp_avg=$(cat <<EOF
+BEGIN {
+	count=0
+	sum=0
+}
+{
+	count++
+	sum += \$1
+}
+END {
+	b = sum / count
+	printf("%d",b+=b<0?0:0.9)
+}
+EOF
+)
+
+# TODO check lengte and make sure we aren't wayy off base
+wifi() {
+	local w sig ssid
+	w=$(ifconfig wlan | grep "ieee80211")
+	ssid="$(echo "$w" | awk "${wifi_ssid}")"
+	sig="$(echo "$w" | awk "${wifi_sig}")"
+  printf "%s(%s)" "${ssid}" "${sig}"
+}
+
+beat() {
+	local b
+	b=$(echo "(($(date +'%s')+3600)%86400)/86.4" | bc)
+	printf "%03d" "${b}"
+}
+
+hz() {
+	local hz
+	hz=$(apm -Pv | \
+		awk \
+		'{gsub(/\(/, "", $5); gsub(/\)/, "", $6); print $5 " " $6 " " $4}')
+	printf "%s" "$hz"
+}
+
+mem() {
+	local mem_info
+	mem_info=$(top -n | grep Mem | awk "${mem_fix}")
+	printf "%s" "${mem_info}"
+}
+
+temp() {
+	local TMP
+	# Average all the sensors
+	TMP=$(sysctl hw.sensors | grep temp | \
+		awk -F= '{print $2}' | \
+		awk "${temp_avg}")
+	if [ $TMP -ge 85 ]; then
+		printf "\\${red}%2d\\${black}°C" "${TMP}"
+	else
+		printf "%2d°C" "${TMP}"
+	fi
+}
+battery() {
+	local BATT BAR BATT_LINE
+	if sysctl -n hw.product | grep -iq pine64; then
+		pct=$(sysctl -n hw.sensors.cwfg0.percent0)
+		pct=${pct%%.*}
+		set -A batt_info $pct 0 0
+	else
+		set -A batt_info $(apm -alm)
+	fi
+
+	BATT=$((${batt_info[0]}/10))
+	BAR="#"
+	if [ "${batt_info[2]}" == "1" ] ; then
+		BAR="+"
+	fi
+
+	BATT_LINE=""
+	for i in $(jot 10); do
+		if [ "$i" -le "$BATT" ]; then
+			BATT_LINE="${BATT_LINE}${BAR}"
+		else
+			BATT_LINE="${BATT_LINE}-"
+		fi
+	done
+
+	if [ $BATT -lt 3 ] && [ "${batt_info[2]}" == "0" ]; then
+		BATT_LINE="${red}${BATT_LINE}${black}"
+	fi
+
+	if [ $BATT -lt 5 ] && [ "${batt_info[2]}" == "0" ]; then
+		BATT_LINE="${yellow}${BATT_LINE}${black}"
+	fi
+	echo "${batt_info[0]}%"
+}
+
+vmm() {
+	if pgrep -q vmd; then
+		set -A running_vms $(vmctl status | grep running | awk '{print $NF"("$5")"}')
+		echo -n "VMs: "
+		for vm in "${running_vms[@]}"; do
+			echo -n "${vm} "
+		done
+	else
+		echo -n ""
+	fi
+}
+
+
+function Battery {
+	ADAPTER=$(/usr/sbin/apm -a)
+	BPERCENT=$(/usr/sbin/apm -l)
+	BMINUTES=$(/usr/sbin/apm -m)
+	CHARGING=$(sysctl -n hw.sensors.acpibat0.raw0 | awk '{print $1}')
+
+	if [ ${ADAPTER} = 0 ] ; then
+		print -n "Batt: "
+	elif [ ${ADAPTER} = 1 ] ; then
+		print -n "AC: "
+	else
+		print -n "AC: "
+	fi
+	if [ ${BPERCENT} -gt 75 ] ; then
+		print -n "${GREEN}${BPERCENT}%${COLOROFF}"
+	elif [ ${BPERCENT} -gt 50 ] ; then
+		print -n "${YELLOW}${BPERCENT}%${COLOROFF}"
+	elif [ ${BPERCENT} -gt 25 ] ; then
+		print -n "${ORANGE}${BPERCENT}%${COLOROFF}"
+	else
+		if [ $CHARGING = 2 ]; then
+			print -n "${BPERCENT}%"
+		else
+			print -n "${RED}${REVERSE}${BPERCENT}${BATTERY}%${COLOROFF}"
+		fi
+	fi
+	[[ "${BMINUTES}" != "unknown" ]] && print -n \
+		" ($((${BMINUTES} / 60))h$((${BMINUTES} % 60))m)${COLOROFF}"
+	[[ ${CHARGING} = 2 ]] && print -n " ${COLOROFF}charging"
+}
+
+function Clock {
+	local DATETIME=$(date "+%a %F %H:%M %Z")
+	print -n "${COLOROFF}${DATETIME}"
+}
+
+
+function Display {
+	local LIGHT=$(xbacklight | awk -F. '{print $1'})
+	print -n "Display: ${LIGHT}%"
+}
+
+
+function Volume {
+	local MUTE=$(sndioctl output.mute | awk -F '=' '{ print $2 }')
+	local SPK="$(sndioctl output.level | awk -F '=' '{ print $2 * 100 }')%"
+  print -n "Vol: "
+	if [ "${MUTE}" = "1" ] ; then
+		SPK="mute"
+	else
+		#print -pn "${GREEN}"
+		print -n ""
+	fi
+	print -n "${SPK}${COLOROFF}"
+}
+
+
+while true ; do
+  printf "%s | %s | %s | CPU: %s | %s %s | WIFI: %s | %s | %s" \
+  "$(Battery)" "$(Display)" "$(Volume)" "$(temp)" "$(mem)" \
+  "$(vmm)" "$(wifi)" "$(Clock)"
+  sleep 5
+done
---
+++ .config/scripts/lemon-bar.sh
@@ -0,0 +1,277 @@
+#!/bin/ksh
+
+#
+# Original script from Peter Hessler via Pamela Mosiejczuk
+#
+# Heavily modified to "de-magic" the color strings and add more comments
+#
+
+# WiFi interface
+WIFI="iwm0"
+
+# Wired Interface
+WIRED="em0"
+
+# Set this to the trunk interface if you are using the trunk wired/wifi trick
+TRUNK="trunk0"
+
+# Lemonbar magic strings for color
+BACKGROUND="%{B#000000}" # Black
+RED="%{F#FF0000}${BACKGROUND}"
+GREEN="%{F#00FF00}${BACKGROUND}"
+YELLOW="%{F#FFFF00}${BACKGROUND}"
+ORANGE="%{F#FFA500}${BACKGROUND}"
+REVERSE="%{R}"
+COLOROFF="%{F-}%{B-}"
+
+# Magic hex characters to put the degrees symbol plus C for celsius
+DEGCELSIUS="°C"
+
+function Battery {
+	ADAPTER=$(/usr/sbin/apm -a)
+	BPERCENT=$(/usr/sbin/apm -l)
+	BMINUTES=$(/usr/sbin/apm -m)
+	CHARGING=$(sysctl -n hw.sensors.acpibat0.raw0 | awk '{print $1}')
+
+	if [ ${ADAPTER} = 0 ] ; then
+		print -pn "batt: "
+	elif [ ${ADAPTER} = 1 ] ; then
+		print -pn "AC: "
+	else
+		print -pn "AC: "
+	fi
+	if [ ${BPERCENT} -gt 75 ] ; then
+		print -pn "${GREEN}${BPERCENT}%%${COLOROFF}"
+	elif [ ${BPERCENT} -gt 50 ] ; then
+		print -pn "${YELLOW}${BPERCENT}%%${COLOROFF}"
+	elif [ ${BPERCENT} -gt 25 ] ; then
+		print -pn "${ORANGE}${BPERCENT}%%${COLOROFF}"
+	else
+		if [ $CHARGING = 2 ]; then
+			print -pn "${BPERCENT}%%"
+		else
+			print -pn "${RED}${REVERSE}${BPERCENT}${BATTERY}%%${COLOROFF}"
+		fi
+	fi
+	[[ "${BMINUTES}" != "unknown" ]] && print -pn \
+		" ($((${BMINUTES} / 60))h$((${BMINUTES} % 60))m)${COLOROFF}"
+	[[ ${CHARGING} = 2 ]] && print -pn " ${COLOROFF}charging"
+}
+
+function Clock {
+	local DATETIME=$(date "+%a %F %H:%M %Z")
+	print -pn "${COLOROFF}${DATETIME}"
+}
+
+function Cpu {
+	set -A cpu_values $(iostat -C | sed -n '3,3p')
+	local CPULOAD=$((100-${cpu_values[5]}))
+	local CPUTEMP=$(sysctl -n hw.sensors.cpu0.temp0 | awk -F. '{print $1'})
+  print -pn "CPU: "
+	if [ ${CPULOAD} -ge 90 ] ; then
+		print -pn "${RED}"
+	elif [ ${CPULOAD} -ge 80 ] ; then
+		print -pn "${YELLOW}"
+	else
+		print -pn "${GREEN}"
+	fi
+	print -pn "${CPUTEMP}${DEGCELSIUS}${COLOROFF}"
+}
+
+function Load {
+	local SYSLOAD=$(systat -b | awk 'NR==3 { print $4" "$5" "$6 }')
+	print -pn "Load: ${SYSLOAD}"
+}
+
+function Display {
+	local LIGHT=$(xbacklight | awk -F. '{print $1'})
+	print -pn "Display: ${LIGHT}%%"
+}
+
+function Music {
+	local STATUS=$(/usr/local/bin/mpc | sed -n '2p' | grep -c paused)
+	local SONG=$(/usr/local/bin/mpc current -f '%artist% - %title%')
+	print -pn "%{A:/usr/local/bin/mpc -q toggle:}"
+	print -pn "%{A3:/usr/local/bin/mpc -q next:}"
+	if [ "$STATUS" -eq "0" ]; then
+		if [ -z "$SONG" ]; then
+			print -pn "Music Stopped"
+		else
+			print -pn "Now Playing: ${SONG}"
+		fi
+	else
+		print -pn "Paused: ${SONG}"
+	fi
+	print -pn "%{A}"
+	print -pn "%{A}"
+}
+
+function Volume {
+	local MUTE=$(sndioctl output.mute | awk -F '=' '{ print $2 }')
+	local SPK="$(sndioctl output.level | awk -F '=' '{ print $2 * 100 }')%%"
+  print -pn "Vol: "
+	if [ "${MUTE}" = "1" ] ; then
+		SPK="mute"
+	else
+		#print -pn "${GREEN}"
+		print -pn ""
+	fi
+	print -pn "${SPK}${COLOROFF}"
+}
+
+i=0
+_w=""
+
+function Weather {
+	if [ "$(($i % 20))" = "0" ]; then
+		_w=$(/usr/local/bin/ansiweather -a false 2>/dev/null)
+		[[ $? != 0 ]] && _w=" No Weather Report"
+		i=0
+	fi
+	let i++
+
+	print -pn "${_w# }"
+}
+
+function Wlan {
+	local WLANSTAT=$(ifconfig ${WIFI} | awk '/status:/ { print $2 }')
+	local WLANID=$(ifconfig ${WIFI}  | \
+		awk -F '(nwid |join | chan)' '/(nwid|join)/ { print $2 }' )
+	local WLANSIG=$(ifconfig ${WIFI} | grep ieee80211 | \
+		awk 'match($0, /-[0-9]*dBm/) \
+		{ print substr($0, RSTART, RLENGTH) }')
+
+	print -pn "WLAN: "
+	if [ "${WLANSTAT}" = "active" ] ; then
+		print -pn "${GREEN}"
+	else
+		print -pn "${RED}"
+	fi
+	print -pn "${WLANID} "
+	if [ "${WLANSTAT}" = "active" ] ; then
+		print -pn -- "${WLANSIG}"
+	else
+		print -pn "offline"
+	fi
+	print -pn "${COLOROFF}"
+}
+
+function Wired {
+	print -pn "Network: wired"
+}
+
+function Network {
+	# find the ports in the trunk and the interface labelled as active
+	local ACTIVE_IFACE=$( ifconfig $TRUNK | grep port | grep active \
+		| awk '{ print $1 }' )
+	if [ x$ACTIVE_IFACE = x$WIRED ] ; then
+		Wired
+	elif [ x$ACTIVE_IFACE = x$WIFI ] ; then
+		Wlan
+	else
+		print -pn "Network: ${YELLOW}Unknown${COLOROFF}"
+	fi
+}
+
+
+wifi_ssid=$(cat <<EOF
+\$1 ~ "ieee80211" {
+	split(\$0, chars, "");
+	match(\$0, /join .* chan/);
+	ssid = substr(\$0, RSTART + 5, RLENGTH - 10)
+	gsub(/\"/, "", ssid)
+	print ssid
+}
+EOF
+)
+wifi_sig=$(cat <<EOF
+\$1 ~ "ieee80211" {
+	split(\$0, chars, "");
+	match(\$0, /[0-9]+%/);
+	val=substr(\$0, RSTART, RLENGTH)
+	if (val == "") {
+		printf \$(NF - 9)
+	} else {
+		printf "%3d%%", val
+	}
+}
+EOF
+)
+
+wifi() {
+	local w sig ssid
+	w=$(ifconfig wlan | grep "ieee80211")
+	ssid="$(echo "$w" | awk "${wifi_ssid}")"
+	sig="$(echo "$w" | awk "${wifi_sig}")"
+	print -pn "${ssid}:${sig}"
+}
+
+
+mem_fix=$(cat <<EOF
+{
+	split(\$3, real, "/")
+	print "Mem: U: " real[2] " F: " \$6 " C: " \$8
+}
+EOF
+)
+
+
+mem() {
+	local mem_info
+	mem_info=$(top -n | grep Mem | awk "${mem_fix}")
+	print -pn "${mem_info}"
+}
+
+# Start lemonbar as a corprocess
+# - Make its X name "lemonbar"
+# so the window manager can be configured to skip putting a border on it
+lemonbar -n lemonbar |&
+
+# Main information loop feed to lemonbar
+while true ; do
+	# Look for restart file
+	if [ -O ~/.newbar ] ; then
+		rm ~/.newbar
+		exec $0
+	fi
+	# Left-justify this group
+	print -pn "%{l}"
+
+	Clock
+	print -pn " | "
+
+	Battery
+	print -pn " | "
+
+	Cpu
+	print -pn " | "
+
+  mem
+	print -pn " | "
+	#Load
+	#print -pn " | "
+
+	Weather
+
+	# The rest are right-justified
+	print -pn "%{r}"
+
+#	Music
+#	print -pn " | "
+
+	Volume
+	print -pn " | "
+
+	Display
+	print -pn " | "
+
+	#Wlan
+  wifi
+	print -p ""
+
+	# Wait 5 seconds between updates
+	sleep 5
+
+	#### Put read/eval part here? Is there nonblocking read?
+done
+#done | lemonbar -d -n lemonbar | while read line; do eval "$line"; done
